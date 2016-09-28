b742042809eca37413c8b9ece3aa223158afdc42
Talon Hughes <talon.hughes@gmail.com>
2016-09-27 18:33:11 -0600

    finsihed mini project

--- controller.js
+++ controller.js
@@ -1,10 +1,20 @@
+///Old controller///
+// angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+//   $scope.getUsers = function() {
+//     $scope.users = mainService.getUsers();
+//   }
+//
+//   $scope.getUsers();
+// });
+
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
-
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
-
-
+    mainService.getUsers().then(function(response) {
+      $scope.users = response.data.data;
+    });
+  };
   $scope.getUsers();
-
 });
+
+//Here we are calling the mainService.getUsers function, and saying that once we finish that function, THEN define $scope.users.
+//The response is a giant JSON object that contains a lot of information we don't necessarily need right now. That's why $scope.users is equal to response.data.data. Data is a child of response, and it has a child named data. The second child named data is where our user profile information lives.
--- service.js
+++ service.js
@@ -1,29 +1,38 @@
 angular.module('userProfiles').service('mainService', function($http) {
+	
+// var data =
+// 	[
+// 	    {
+// 	        "id": 1,
+// 	        "first_name": "george",
+// 	        "last_name": "bluth",
+// 	        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
+// 	    },
+// 	    {
+// 	        "id": 2,
+// 	        "first_name": "lucille",
+// 	        "last_name": "bluth",
+// 	        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
+// 	    },
+// 	    {
+// 	        "id": 3,
+// 	        "first_name": "oscar",
+// 	        "last_name": "bluth",
+// 	        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
+// 	    }
+// 	  ];
+//
+// 		this.getUsers = function(){
+// 			return data;
+// 		};
 
-	var data = [
-    {
-        "id": 1,
-        "first_name": "george",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
-    },
-    {
-        "id": 2,
-        "first_name": "lucille",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
-    },
-    {
-        "id": 3,
-        "first_name": "oscar",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
-    }
-  ];
-
-  this.getUsers = function() {
-    return data;
+	this.getUsers = function() {
+    return $http({
+			method: 'GET',
+			url: 'http://reqres.in/api/users?page=1'
+		});
   };
-
-
 });
+
+
+//The METHOD part tells the API we're hitting what kind of request we are making. There are a lot of different types of requests. Here we are getting data.
